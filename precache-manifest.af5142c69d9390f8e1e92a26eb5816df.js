1fa2edfec130153ac5922ab2fc4732b6e4bf4b42
K0shal <80113133+K0shal@users.noreply.github.com>
2022-11-11 15:42:42 +0530

    Add files via upload

---
+++ precache-manifest.af5142c69d9390f8e1e92a26eb5816df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a44de9d9149a484fbcb3550e708c40",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "b3783b1e6fa1996b1066",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "4e572692a5fc268f9dbf",
+    "url": "/box-office/static/js/2.5bbfd9d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.5bbfd9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3783b1e6fa1996b1066",
+    "url": "/box-office/static/js/main.d6a7c376.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
